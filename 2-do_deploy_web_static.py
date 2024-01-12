206da33fdd23414b25183472590dfa03e773ff0a
MD-MADARA <mouadotaku9421@gmail.com>
2024-01-12 03:39:07 +0100

    do deploy v1

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""Fabric script (based on the file 1-pack_web_static.py) that distributes
+an archive to your web servers,using the function do_deploy
+"""
+from fabric.api import *
+from fabric.operations import put
+from datetime import datetime
+import os
+
+
+def do_pack():
+    """packages all contents from web_static into .tgz archive
+    """
+    time = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('mkdir versions')
+    result = local(f'tar -cvf versions/web_static_{time}.tgz web_static')
+    if result.failed:
+        return None
+    else:
+        return result
+
+
+env.hosts = ["34.229.66.3", "100.26.234.152"]
+
+
+def do_deploy(archive_path):
+    """Deploys a static archive to my web servers"""
+
+    if not os.path.isfile(archive_path):
+        print('archive file does not exist...')
+        return False  # Returns False if the file at archive_path doesnt exist
+    try:
+        archive = archive_path.split('/')[1]
+        no_archive_ext = archive.split('.')[0]
+    except Exception:
+        print('failed to get archive name from split...')
+        return False
+
+    uploaded = put(archive_path, '/tmp/')
+    if uploaded.failed:
+        return False
+    Res = run('mkdir -p /data/web_static/releases/{}/'.format(no_archive_ext))
+    if Res.failed:
+        print('failed to create archive directory for relase...')
+        return False
+    Res = run('tar -C /data/web_static/releases/{} -xzf /tmp/{}'.format(
+               no_archive_ext, archive))
+    if Res.failed:
+        print('failed to untar archive...')
+        return False
+    Res = run('rm /tmp/{}'.format(archive))
+    if Res.failed:
+        print('failed to remove archive...')
+        return False
+    Res = run('mv /data/web_static/releases/{}/web_static/* \
+               /data/web_static/releases/{}'
+              .format(no_archive_ext, no_archive_ext))
+    if Res.failed:
+        print('failed to move extraction to proper directory...')
+        return False
+    Res = run('rm -rf /data/web_static/releases/{}/web_static'
+              .format(no_archive_ext))
+    if Res.failed:
+        print('failed to remove first copy of extraction after move...')
+        return False
+
+    # clean up old release and remove it.
+
+    Res = run('rm -rf /data/web_static/current')
+    if Res.failed:
+        print('failed to clean up old release...')
+        return False
+    Res = run('ln -sfn /data/web_static/releases/{} /data/web_static/current'
+              .format(no_archive_ext))
+    if Res.failed:
+        print('failed to create link to new release...')
+        return False
+
+    print('\nNew Version Successfuly Deployed!\n')
+
+    return True
