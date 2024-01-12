5d7d606aea72ec07e5ba44ca437212aa7fe42f55
MD-MADARA <mouadotaku9421@gmail.com>
2024-01-12 22:17:38 +0100

    clean

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,54 +1,30 @@
 #!/usr/bin/python3
-"""Fabric script (based on the file 1-pack_web_static.py) that distributes
-an archive to your web servers,using the function do_deploy
-"""
+"""Deploy archive!"""
 from fabric.api import env, put, run
-from  os import path
-
+from os.path import exists
 env.hosts = ["34.229.66.3", "100.26.234.152"]
 
 
 def do_deploy(archive_path):
-    """Deploys a static archive to my web servers"""
-
-    if not path.isfile(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
     try:
-        archive_tgz = archive_path.split('/')[1]
-        archive_no_ext = archive_tgz.split('.')[0]
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+        dest_path = f"/data/web_static/releases/{no_ext}"
+        if (
+            put(archive_path, '/tmp/').failed or
+            run(f'mkdir -p {dest_path}/').failed or
+            run(f'tar -xzf /tmp/{file_name} -C {dest_path}/').failed or
+            run(f'rm /tmp/{file_name}').failed or
+            run(f'mv {dest_path}/web_static/* {dest_path}/').failed or
+            run(f'rm -rf {dest_path}/web_static').failed or
+            run(f'rm -rf /data/web_static/current').failed or
+            run(f'ln -s {dest_path}/ /data/web_static/current').failed
+        ):
+            return False
     except Exception:
         return False
-
-    if put(archive_path, '/tmp/').failed is True:
-        return False
-
-    if run('mkdir -p /data/web_static/releases/{}/'.format(
-        archive_no_ext)).failed is True:
-        return False
-
-    if run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(
-               archive_tgz, archive_no_ext)).failed is True:
-        return False
-
-    if run('rm /tmp/{}'.format(archive_tgz)).failed is True:
-        return False
-
-    if run('mv /data/web_static/releases/{}/web_static/* \
-               /data/web_static/releases/{}'
-              .format(archive_no_ext, archive_no_ext)).failed is True:
-        return False
-
-    if run('rm -rf /data/web_static/releases/{}/web_static'
-              .format(archive_no_ext)).failed is True:
-        return False
-
-    if run('rm -rf /data/web_static/current').failed is True:
-        return False
-
-    if run('ln -s /data/web_static/releases/{} /data/web_static/current'
-              .format(archive_no_ext)).failed is True:
-        return False
-
-    print('\nNew version deployed!\n')
-
+    print("New version deployed!")
     return True
