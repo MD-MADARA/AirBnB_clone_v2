211d8c4c2ce5a7c91e09a91f006a388253368cf0
MD-MADARA <mouadotaku9421@gmail.com>
2024-01-27 23:54:32 +0100

    fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -41,7 +41,7 @@ def number(n):
 
 @app.route('/number_template/<int:n>')
 def number_template(n):
-    return render_template('5-number.html', num=n)
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
